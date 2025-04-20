95d7e66e3f7aa4cd7782806e5c0985f6d38ff457
Saihaan Khan <saihaan57@gmail.com>
2025-04-20 23:24:53 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {  
   res.send('Hello World!')
@@ -18,6 +20,6 @@ app.get('/youtube' , (req,res) => {
     res.send('HEY!!')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
