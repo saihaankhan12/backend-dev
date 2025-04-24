f3f52b1e0c98109efa0a85a0eaf12e1cb83a451e
Saihaan Khan <saihaan57@gmail.com>
2025-04-24 11:43:30 +0530

    github data

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000
+const githubData = {
+  "login": "saihaankhan12",
+  "id": 138798531,
+  "node_id": "U_kgDOCEXlww",
+  "avatar_url": "https://avatars.githubusercontent.com/u/138798531?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/saihaankhan12",
+  "html_url": "https://github.com/saihaankhan12",
+  "followers_url": "https://api.github.com/users/saihaankhan12/followers",
+  "following_url": "https://api.github.com/users/saihaankhan12/following{/other_user}",
+  "gists_url": "https://api.github.com/users/saihaankhan12/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/saihaankhan12/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/saihaankhan12/subscriptions",
+  "organizations_url": "https://api.github.com/users/saihaankhan12/orgs",
+  "repos_url": "https://api.github.com/users/saihaankhan12/repos",
+  "events_url": "https://api.github.com/users/saihaankhan12/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/saihaankhan12/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 8,
+  "public_gists": 0,
+  "followers": 2,
+  "following": 8,
+  "created_at": "2023-07-06T14:50:01Z",
+  "updated_at": "2025-04-20T17:56:56Z"
+}
 
 app.get('/', (req, res) => {  
   res.send('Hello World!')
@@ -20,6 +55,11 @@ app.get('/youtube' , (req,res) => {
     res.send('HEY!!')
 })
 
+app.get('/github' , (req,res) => {
+  res.json(githubData)
+})
+
+
 app.listen(process.env.port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.port}`)
 })
